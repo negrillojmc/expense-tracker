07eb5e8d0d076e878e8eb6760723653e33b92580
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2025-05-09 02:19:12 +0800

    wrote auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,7 +44,43 @@ exports.registerUser = async (req, res) => {
 
 
 // login User
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    
+    res.status(200).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
 
-// Register User
-exports.getUserInfo = async (req, res) => {};
+// Get User Info
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: 
+        "User not found!"
+      });
+    }
+
+    res.status(200).json(user)
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching user info."})
+  }
+};
